ae2d12358f804ccbfeaf9b4dead9e27df1e7cdd8
Fabien Walle <fabienwalle13@gmail.com>
2024-11-15 19:33:41 +0100

    auth functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@nextui-org/react";
+import * as actions from '@/actions'
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
   return (
     <div>
-      <Button>Click me</Button>
+      <form action={actions.SignIn}>
+        <Button type="submit">SignIn</Button>
+      </form>
+      <form action={actions.SignOut}>
+        <Button type="submit">SignOut</Button>
+      </form>
+
+      {
+        session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed Out</div>
+      }
+      <Profile />
     </div>
   );
 }
