70c3c5d0b54ef111d1204dd8326629c25c053016
Fabien Walle <fabienwalle13@gmail.com>
2024-11-18 14:49:47 +0100

    create topic with errors handlings

---
+++ src/components/topics/topic-create-form.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Button, Input, Popover, PopoverContent, PopoverTrigger, Textarea } from "@nextui-org/react";
+import * as actions from '@/actions'
+import { useActionState } from "react";
+
+export default function TopicCreateForm() {
+    const [formState, action] = useActionState(actions.createTopic, { errors: {} })
+
+    return (
+        <Popover placement="left">
+            <PopoverTrigger>
+                <Button color="primary">Create a Topic</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <form action={action}>
+                    <div className="flex flex-col gap-4 p-4 w-80">
+                        <h3 className="text-lg">
+                            Create a Topic
+                        </h3>
+                        <Input
+                            label="Name"
+                            name="name"
+                            labelPlacement="outside"
+                            placeholder="Name"
+                            isInvalid={!!formState.errors.name}
+                            errorMessage={formState.errors.name?.join(', ')} />
+                        <Textarea
+                            label="Description"
+                            name="description"
+                            labelPlacement="outside"
+                            placeholder="Describe your topic"
+                            isInvalid={!!formState.errors.description}
+                            errorMessage={formState.errors.description?.join(', ')} />
+                        <Button type="submit">Create</Button>
+                        {formState.errors._form 
+                        ? <div className="p-2 bg-red-200 border border-red-400 rounded">{formState.errors._form?.join(', ')}</div>
+                        : null}
+                    </div>
+                </form>
+            </PopoverContent>
+        </Popover>
+    )
+}
